528bade8dd48299761c50d3f263e06e374310cd5
Azizbek <azizbek07@gmail.com>
2025-05-23 01:14:43 +0200

    Delete url-shortener-terraform directory

--- url-shortener-terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "api_gateway_base_url" {
-  value = "https://${aws_api_gateway_rest_api.url_api.id}.execute-api.${var.aws_region}.amazonaws.com/prod"
-}
-
-output "s3_static_website_url" {
-  value = aws_s3_bucket_website_configuration.frontend_website.website_endpoint
-}
